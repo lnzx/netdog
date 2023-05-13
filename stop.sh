40dd5fefebaf3c146508c4afeff366bfa68732be
lnzx <1515417+lnzx@users.noreply.github.com>
2023-05-13 10:38:27 +0800

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sudo docker kill --signal=SIGTERM saturn-node
-echo 'wait for 30 minutes to drain all requests'
-sleep 1800
-sudo docker stop saturn-node
+sudo docker compose down
 echo 'stop docker saturn ok'
 
